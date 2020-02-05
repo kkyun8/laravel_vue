26305f4cb9169dbe22e475ae3bcf310db7da90ce
kkyun8 <kim23302@icloud.com>
2020-02-05 13:14:06 +0900

    update controller, route

--- web/laravel/app/Http/Controllers/Auth/LoginController.php
+++ web/laravel/app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request; // TODO: request 必須
+
+
 class LoginController extends Controller
 {
     /*
@@ -36,5 +39,17 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        return $user;
+    }
+
+    protected function loggedOut(Request $request)
+    {
+      $request->session()->regenerate();
+
+      return response()->json();
     }
 }
