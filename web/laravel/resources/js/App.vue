f7d283f773a87cfa025d1df6da16c4b4a8d760cb
kkyun8 <kim23302@icloud.com>
2020-02-05 13:15:38 +0900

    create component, page, util

--- web/laravel/resources/js/App.vue
+++ web/laravel/resources/js/App.vue
@@ -7,3 +7,15 @@
     </main>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navvar'
+import Footer from './components/Footer'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
