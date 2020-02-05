b5e9a2968d0b9ef52fb449986e5b44e66965ce7f
kkyun8 <kim23302@icloud.com>
2020-02-05 13:35:58 +0900

    mastar pull -> push

---
+++ web/laravel/resources/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <main>
+      <div class="container">
+        <RouterView />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navvar'
+import Footer from './components/Footer'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
